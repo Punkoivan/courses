69e2bc7cb671518a3d8f44a019ecb49939f5e429
ivan <punkoivan@yandex.ru>
2016-06-27 22:34:15 +0300

    Revert "demof"
    This reverts commit a2dcd209fee031bc3b73057ec2bf238028b05777.

--- demof/auto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-STRING=$(grep -n "end" Vagrantfile |tail -n 3|head -n 1 |grep -o -e "[0-9]*")
-NEWSTRING=$((STRING+2))
-#echo $NEWSTRING
-
-sed -i "${NEWSTRING}i\$(grep -A 2 "n.*3" Vagrantfile| sed -e "s/3/4/g")" Vagrantfile
