544488e84820b8bde9d65e7e8b25cbd4b594459e
ivan <punkoivan@yandex.ru>
2016-06-28 21:53:46 +0300

    port parsing

--- demof/dumb.sh
+++ demof/dumb.sh
@@ -4,6 +4,8 @@ STRING=$(grep -n "end" Vagrantfile |tail -n 3|head -n 1 |grep -o -e "[0-9]*")
 NEW_STRING=$((STRING+2))
 NEW_NODE=$((LAST_NODE+1))
 VAGRANT_HOME=./Vagrantfile
+NEW_NODE_IP=$(tac ${VAGRANT_HOME} | grep -m 1 -o -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+PORT=$(tac Vagrantfile | grep -m 1 -e "guest" | grep -o -E "[0-9]{4}")
 sed -i '$d' ${VAGRANT_HOME}
 cat ${VAGRANT_HOME} |grep -A 2 node${LAST_NODE} | sed -e "s/${LAST_NODE}/${NEW_NODE}/g" >> ${VAGRANT_HOME}
 echo end >> ${VAGRANT_HOME}
